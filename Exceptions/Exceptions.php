39a95bc852c6105e4773b2e6e81e8d7135f049d5
Mogria <m0gr14@gmail.com>
2012-02-09 09:48:58 +0100

    Initial Commit

---
+++ Exceptions/Exceptions.php
@@ -0,0 +1,8 @@
+<?php
+namespace Void;
+
+class VoidException extends \Exception{}
+
+class UndefinedPropertyException extends VoidException {}
+
+class InexistentFileException extends VoidException {}
