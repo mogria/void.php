ade5db5891309e931847bfaa546730f843782735
Mogria <m0gr14@gmail.com>
2012-02-09 10:50:09 +0100

    Added Job and JobCollection class

---
+++ Job/JobCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Void;
+
+class JobCollection implements Job {
+  
+  protected $jobs;
+  
+  public function add(Job $job) {
+    $this->jobs[] = $job;
+  }
+  
+  public function remove(Job $job) {
+    array_diff($this->jobs, array($job));
+  }
+  
+  public function run() {
+    foreach($this->jobs as $job) {
+      $job->run();
+    }
+  }
+  
+  public function cleanup() {
+    foreach($this->jobs as $job) {
+      $job->cleanup();
+    }
+  }
+}
