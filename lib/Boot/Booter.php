c372edafe33db880a8233522371673f21fbefc0e
Mogria <m0gr14@gmail.com>
2012-02-09 12:27:04 +0100

    added Booter class

---
+++ lib/Boot/Booter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Void;
+
+class Booter extends JobCollection {
+  static protected $instance;
+  
+  static protected $jobs_dir = "Jobs";
+  
+  protected function __construct() {
+    
+  }
+  
+  protected function __clone() {}
+  
+  public static function boot() {
+    if(self::$instance instanceof Booter) {
+      throw new \BadMethodCallException("already booted!");
+    }
+    self::$instance = new Booter();
+    self::$instance->run();
+  }
+  
+  public static function shutdown() {
+    if(!self::$instance instanceof Booter) {
+      throw new \BadMethodCallException("You need to " . __CLASS__ . "::boot() before you can call " . __METHOD__);
+    }
+    self::$instance->cleanup();
+    self::$instance = null;
+  }
+}
