486cebe11c8134cb8ba78db827a6cbb4bdbe6def
Mogria <m0gr14@gmail.com>
2012-02-17 01:47:41 +0100

    commented index.php

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <?php
+/**
+ * @author Mogria
+ * @package Void.php
+ *
+ * A Simple MVC Framework written in PHP.
+ */
 
+// All the files of the framework use this namespace
 namespace Void;
 
+// This defines some useful constants
 require_once 'config/consts.php';
+// For automated loading of classes
 require_once 'lib/Autoloader/Autoloader.php';
 
+// Startup
 Booter::boot();
 
+// Read the request
 $request = new Request();
 
+// instanciate the Dispatcher
 $dispatcher = new Dispatcher($request);
 
+// Get an instance of the Controller
 $controller = $dispatcher->getController();
 
+// Execute the action according to the request
 echo $controller->executeAction($dispatcher);
 
+// Clean up
 Booter::shutdown();
