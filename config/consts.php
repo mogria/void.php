a144d8679ce9dbf9b465c0c5238847945cf8df67
Mogria <m0gr14@gmail.com>
2012-02-17 01:47:58 +0100

    commented config/consts.php

--- config/consts.php
+++ config/consts.php
@@ -1,5 +1,6 @@
 <?php
 
+// The URL to the location where this framework is
 define('BASEURL', 
     'http' . (!empty($_SERVER['HTTPS']) ? "s" : "")  . '://' 
     . $_SERVER['HTTP_HOST']
@@ -7,7 +8,9 @@ define('BASEURL',
     . dirname($_SERVER['SCRIPT_NAME'])
 );
 
+// the shorter the better ;-)
 define('DS', DIRECTORY_SEPARATOR);
 
+// the path in the file system to the framework
 define('ROOT', realpath('.') . DS);
 
