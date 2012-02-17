b369acc8ac75b0ecd05c82ec495a9169d838fbc3
Mogria <m0gr14@gmail.com>
2012-02-17 05:04:30 +0100

    Commented the view

--- lib/View/Template.php
+++ lib/View/Template.php
@@ -5,29 +5,61 @@
 
 namespace Void;
 
+/**
+ * A Template System
+ */
 class Template extends VirtualAttribute {
+  /**
+   * @var string $file
+   */
   protected $file;
-  
+
+  /**
+   * Constructor
+   * @param string $file
+   * @param Array $initializers
+   */
   public function __construct($file, Array $initializers = Array()) {
     $this->setFile($file);
     $this->_ = $initializers;
   }
-  
+
+  /**
+   * this sets the template file which should be used.
+   * You can also pass an Array, see the TemplateFinder class
+   *
+   * @param mixed $file
+   * @see TemplateFinder
+   */
   public function setFile($file) {
     $finder = new TemplateFinder($file);
     $this->file = $finder->getPath();
   }
-  
+
+  /**
+   * returns the path to the used template file
+   *
+   * @return string
+   */
   public function getFile() {
     return $this->file;
   }
-  
+
+  /**
+   * Convert the Template Synatx in the Template files to PHP
+   * within these brackets {} you can simply use PHP Code
+   * if you use {= } the text will be echo'ed out.
+   * if you use {> } the text will be echo'ed out safely.
+   * if you use {[ } an other template object is rendered in place
+   * @param string $string
+   * @return string
+   */
   public function parse($string) {
     return preg_replace(Array(
       '/\{>(.*?)\}/',
       '/\{\[(.*?)\}/',
       '/\{=(.*?)\}/',
-      '/\{(.*?)\}/ms'
+      '/\{(.*?)\}/'
     ), Array(
       "<?php print(htmlspecialchars(\\1)) ?>",
       "<?php \\1->parse() ?>",
@@ -35,7 +67,14 @@ class Template extends VirtualAttribute {
       "<?php \\1 ?>"
     ), $string);
   }
-  
+
+  /**
+   * the template file set will be rendered. All the
+   * variables set (by using the method's from virtual attribute)
+   * are available in the Template file.
+   *
+   * @return string the output of the template
+   */
   public function render() {
     extract($this->toArray());
     ob_start();
--- lib/View/VirtualAttribute.php
+++ lib/View/VirtualAttribute.php
@@ -5,53 +5,115 @@ namespace Void;
  * @author Mogria
  */
 
+/**
+ * An Simple object which is an array and a Iterator at the same time
+ * You can set and get what property you ever want
+ *
+ */
 class VirtualAttribute implements \ArrayAccess, \IteratorAggregate {
+  /**
+   * Array where all the properties are stored
+   * @var Array $__virtual_vars
+   */
   protected $__virtual_vars;
 
+  /**
+   * Constructor
+   *
+   * @param Array $reference
+   */
   public function __construct(&$reference) {
     $this->__virtual_vars = &$reference;
   }
-  
+
+  /**
+   * Throws an Exception if the $key isn't defined
+   *
+   * @param $key
+   */
   protected function isUndefinedProperty($key) {
     if(!$this->exists($key)) {
         throw new UndefinedPropertyException();
     }
   }
 
+  /**
+   * Returns a Reference to the value of $key
+   *
+   * @param $key
+   * @return &mixed
+   */
   public function &get($key) {
     $this->isUndefinedProperty($key);
     return $this->__virtual_vars[$key];
   }
-  
+
+  /**
+   * sets the value of $key
+   *
+   * @param string $key
+   * @param mixed $value
+   * @return mixed
+   */
   public function set($key, $value) {
     return $this->__virtual_vars[$key] = $value;
   }
-  
+
+  /**
+   * sets multiple values
+   *
+   * @param Array $array
+   */
   public function setArray(Array $array) {
     foreach($array as $key => $value) {
       $this->set($key, $value);
     }
   }
 
+  /**
+   * checks if $key exists
+   *
+   * @param string $key
+   * @return bool
+   */
   public function exists($key) {
     return array_key_exists($key, $this->__virtual_vars);
   }
-  
+
+  /**
+   * Removes the $key
+   *
+   * @param string $key
+   */
   public function delete($key) {
     $this->isUndefinedProperty($key);
     unset($this->__virtual_vars[$key]);
   }
-  
+
+  /**
+   * Converts this object to an Array
+   *
+   * @return Array
+   */
   public function toArray() {
     return $this->__virtual_vars;
   }
-  
+
+  /**
+   * Returns a Reference of the array all the variables are stored in
+   *
+   * @return &Array
+   */
   public function &getReference() {
     return $this->__virtual_vars;
   }
-  
+
   /* ArrayAccess */
-  
+
+  /*
+   * The following four method's are declared in the ArrayAccess
+   * and mapped to the corresponding functions in this class
+   */
   public function offsetExists($offset) {
     return $this->exists($offset);
   }
@@ -75,6 +137,10 @@ class VirtualAttribute implements \ArrayAccess, \IteratorAggregate {
 
   /* Magic Methods */
 
+
+  /*
+   * magic function for easier access
+   */
   public function &__get($key) {
     return $this->get($key);
   }
