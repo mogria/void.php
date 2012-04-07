a84471b84cb8a1e3ea7d88ea3a3a626df8299253
Mogria <m0gr14@gmail.com>
2012-04-08 01:23:34 +0200

    added some more tests to the ConfigTest class

--- test/ConfigTest.php
+++ test/ConfigTest.php
@@ -3,16 +3,23 @@
 namespace Void;
 
 require_once 'config/consts.php';
-require_once 'lib/Base/VoidBase.php';
-require_once 'lib/View/VirtualAttribute.php';
-require_once 'lib/Config/Config.php';
+require_once 'autoload.php';
+
+$config = new Config(TEST, function($cfg) {
+  $cfg->test = 15;
+  $cfg->testconfigurable_test = "asdasd";
+});
+
 
 class ConfigTest extends \PHPUnit_Framework_TestCase {
   protected $config;
 
   public function setUp() {
+    global $config;
     // create a simple config in the test environment
-    $this->config = new Config(TEST);
+    $this->config = clone $config; 
+    $this->configurable = new TestConfigurable();
+    VoidBase::setConfig($this->config);
   }
 
   public function testIsEnvironment() {
@@ -43,6 +50,101 @@ class ConfigTest extends \PHPUnit_Framework_TestCase {
     $this->assertFalse("some random value 15" == $this->config->getEnvironment());
   }
 
- /*  public function testConvertClassName() {
- }*/
+  public function testConvertClassName() {
+    $this->assertEquals("void_ness_shitclass", Config::convertClassName("Void\\Ness\\ShitClass"));
+  }
+
+  public function testConcatKeys() {
+    $this->assertEquals("weisch_we_rockts", Config::concatKeys("weisch", "we", "rockts"));
+    $this->assertEquals("weisch_we_rockts", Config::concatKeys("", "weisch", "we", "", "rockts"));
+    $this->assertEquals("weisch_we_rockts", Config::concatKeys(array("weisch", "we", "rockts")));
+    $this->assertEquals("", Config::concatKeys());
+    $this->assertEquals("", Config::concatKeys(""));
+    $this->assertEquals("", Config::concatKeys("", ""));
+    $this->assertEquals("", Config::concatKeys(array()));
+    $this->assertEquals("", Config::concatKeys(array("")));
+    $this->assertEquals("", Config::concatKeys(array("", "")));
+  }
+
+  public function testCalledClass() {
+    /* $this->config->config(function($cfg) {
+      $cfg->config(function($cfg) {
+
+      }, 'all');
+    }); */
+    $this->configurable->testCalledClass($this);
+  }
+
+  public function testGet() {
+    $this->configurable->testGet($this);
+  }
+
+  public function testSet() {
+    $this->configurable->testSet($this);
+  }
+
+  public function testConfig() {
+    $this->configurable->testConfig($this);
+  }
+}
+
+
+class TestConfigurable extends VoidBase {
+  public function testCalledClass($test) {
+    $test->assertEquals("TestConfigurable", Config::getCalledClass());
+  }
+
+  public function testGet($test) {
+    $test->assertEquals("asdasd", self::$config->test);
+  }
+
+  public function testSet($test) {
+    self::$config->void = "trololol";
+    self::$config->test = "dsadsa";
+
+    $test->assertEquals("trololol", self::$config->void);
+    $test->assertEquals("dsadsa", self::$config->test);
+  }
+
+  public function testConfig($test) {
+    self::$config->config(function($cfg) {
+      $cfg->config(function($cfg) {
+        $cfg->all = "test_value";
+      }, 'all');
+
+      $cfg->config(function($cfg) {
+        $cfg->all = "override_value";
+        $cfg->test = "explain";
+      }, DEVELOPMENT);
+
+      $cfg->config(function($cfg) {
+      }, TEST);
+
+      $cfg->config(Array(
+        'key_in_production' => 123,
+        'test' => "hui"
+      ), PRODUCTION);
+    });
+
+    $test->assertEquals(null, self::$config->inexistent_key);
+    $test->assertEquals("test_value", self::$config->all);
+
+    // testing DEVELOMENT environment
+    self::$config->setEnvironment(DEVELOPMENT);
+    $test->assertEquals("override_value", self::$config->all);
+    $test->assertEquals("explain", self::$config->test);
+    $test->assertEquals(null, self::$config->key_in_production);
+
+    // testing TEST environment
+    self::$config->setEnvironment(TEST);
+    $test->assertEquals("asdasd", self::$config->test);
+    $test->assertEquals("test_value", self::$config->all);
+    $test->assertEquals(null, self::$config->key_in_production);
+
+
+    self::$config->setEnvironment(PRODUCTION);
+    $test->assertEquals("hui", self::$config->test);
+    $test->assertEquals("test_value", self::$config->all);
+    $test->assertEquals(123, self::$config->key_in_production);
+  }
 }
