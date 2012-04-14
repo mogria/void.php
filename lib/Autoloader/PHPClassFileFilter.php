7c1586d7131a0b70bc4893daebc69c52e6dbf4d0
Mogria <m0gr14@gmail.com>
2012-04-14 15:55:44 +0200

    added possibility to exclude certain directories from being autoloaded

--- lib/Autoloader/PHPClassFileFilter.php
+++ lib/Autoloader/PHPClassFileFilter.php
@@ -5,14 +5,32 @@ namespace Void;
 use FilterIterator;
 
 class PHPClassFileFilter extends FilterIterator {
+  protected $excluded_dirs = Array(
+    "lib/Model"
+  );
   public function accept() {
     $ext = ".php";
     $value = $this->getInnerIterator()->current();
     $file = $value->getFilename();
     return $value->isFile()
-      && ucfirst(basename($file)) == basename($file)
+      && ucfirst($file) == $file
       && ($position = strrpos($file, $ext)) !== false
       && strlen($file) - strlen($ext) == $position
+      && !$this->inExcludedDir($value->getPath());
       ;
+  }
+
+  public function inExcludedDir($file) {
+    $inside = false;
+    foreach($this->excluded_dirs as $dir) {
+      while($file != DS && $file != "." && !$inside) {
+        // echo str_pad($file, 80, " ", STR_PAD_RIGHT) . ": " . str_pad(substr($file, -strlen($dir)), strlen($dir), " ", STR_PAD_RIGHT) . " === " . $dir . "\n";
+        if(substr($file, -strlen($dir)) === $dir) {
+          $inside = true;
+        }
+        $file = dirname($file);
+      }
+    }
+    return $inside;
   }
 }
