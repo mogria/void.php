eb660dfd79b3be4471a6814784351ef159f87f65
Mogria <m0gr14@gmail.com>
2012-04-16 14:49:04 +0200

    fixed a bug with the scope of self and closures

--- lib/Boot/Jobs/ModelConfig.php
+++ lib/Boot/Jobs/ModelConfig.php
@@ -10,13 +10,14 @@ class ModelConfig extends VoidBase implements Job {
   public function run() {
     define('PHP_ACTIVERECORD_AUTOLOAD_DISABLE', 1);
     require_once ROOT . "lib" . DS . "Model" . DS . "ActiveRecord.php";
-    \ActiveRecord\Config::initialize(function($cfg) {
-      $cfg->set_model_directory(self::$config->dir);
+    $config = self::$config;
+    \ActiveRecord\Config::initialize(function($cfg) use ($config) {
+      $cfg->set_model_directory($config->dir);
       $cfg->set_connections(
         Array(
-          DEVELOPMENT => self::$config->get('connection', DEVELOPMENT),
-          TEST        => self::$config->get('connection', TEST),
-          PRODUCTION  => self::$config->get('connection', PRODUCTION)
+          DEVELOPMENT => $config->get('connection', DEVELOPMENT),
+          TEST        => $config->get('connection', TEST),
+          PRODUCTION  => $config->get('connection', PRODUCTION)
       ));
     });
   }
