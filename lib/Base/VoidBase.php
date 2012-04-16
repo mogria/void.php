2f7fe2af1ca6eb83178e968ea394178142e186fa
Mogria <m0gr14@gmail.com>
2012-04-16 18:33:03 +0200

    commented the VoidBase Class

--- lib/Base/VoidBase.php
+++ lib/Base/VoidBase.php
@@ -2,8 +2,31 @@
 
 namespace Void;
 
+/**
+ * This is the base class of most of the classes in this framework
+ *
+ * This gives the classes the ability to get configured, because the $config
+ * variable is shared to all subclasses
+ */
 abstract class VoidBase {
-  static $config;
+
+  /**
+   * the config variable which contains an Config object with diffrent values in it
+   *
+   * @static
+   * @access protected
+   * @var Config $config
+   */
+  protected static $config;
+
+
+  /** 
+   * The Setter for the $config attribute
+   *
+   * @static
+   * @access public
+   * @param Config $config - the new Config object
+   */
   public static function setConfig($config) {
     static::$config = $config;
   }
