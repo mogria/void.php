177c8ed1926232ccc1fcbd174511d2abb7d9cd51
Mogria <m0gr14@gmail.com>
2012-05-04 12:28:25 +0200

    created test for the Hash Class

---
+++ test/HashTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Void;
+
+require_once 'config/consts.php';
+require_once 'autoload.php';
+
+VoidBase::setConfig($config = new Config(DEVELOPMENT, function($cfg) {
+  $cfg->hash_iterations = 42;
+  $cfg->hash_algo = 'whirlpool';
+  $cfg->hash_secret = 'DAFUQ!';
+}));
+
+class HashTest extends \PHPUnit_Framework_TestCase {
+
+  public function setUp() {
+  }
+
+  public function testGenerateSalt() {
+    $salt = Hash::generateSalt();
+    $this->assertEquals(32, strlen($salt));
+    $this->assertEquals(1, preg_match("/^[0-9a-f]*/D", $salt));
+
+    $this->assertNotEquals(Hash::generateSalt(), $salt);
+  }
+
+  public function testCreate() {
+    $hash = Hash::create("FUS RO DAH!");
+    $this->assertEquals(128 + 1 + 32, strlen($hash));
+    $this->assertEquals(1, preg_match("/^[0-9a-f]*:[0-9a-f]*$/D", $hash));
+
+    $hash = Hash::create("FUS RO DAH!", '8603a0f84c5ee600a42725c12cf22e84');
+    $this->assertEquals(128 + 1 + 32, strlen($hash));
+    $this->assertEquals(1, preg_match("/^[0-9a-f]*:8603a0f84c5ee600a42725c12cf22e84$/D", $hash));
+  }
+
+  public function testCompare() {
+    $hash = Hash::create("FUS RO DAH!");
+    $hash2 = Hash::create("FUS RO DAH!!!!!!!!!");
+
+    $this->assertTrue(Hash::compare("FUS RO DAH!", $hash));
+    $this->assertTrue(Hash::compare("FUS RO DAH!!!!!!!!!", $hash2));
+    $this->assertFalse(Hash::compare("FUS RO DAH!", $hash2));
+    $this->assertFalse(Hash::compare("WHEEEEEEE", $hash));
+  }
+
+}
