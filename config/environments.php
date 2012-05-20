601c1718eae318965cb3e3282ebb4bad89b49aba
Mogria <m0gr14@gmail.com>
2012-05-20 23:44:59 +0200

    updated the database configuration

--- config/environments.php
+++ config/environments.php
@@ -35,19 +35,19 @@ function getConfig() {
     // the settings just for the development environment
     $cfg->config(function($cfg) {
       /** database configuration **/
-      $cfg->modelconfig_connection = 'mysql://root@localhost/development_database_name';
+      $cfg->modelconfig_connection = 'mysql://root@localhost/voidphp_test_blog';
     }, DEVELOPMENT);
 
 
     // the settings for the test environment
     $cfg->config(function($cfg) {
-      $cfg->modelconfig_connection = 'mysql://root@localhost/test_database_name';
+      $cfg->modelconfig_connection = 'mysql://root@localhost/voidphp_test_blog';
     }, TEST);
 
 
     // the settings for the production environment
     $cfg->config(function($cfg) {
-      $cfg->modelconfig_connection = 'mysql://root@localhost/production_database_name';
+      $cfg->modelconfig_connection = 'mysql://root@localhost/voidphp_test_blog';
     }, PRODUCTION);
   });
 }
