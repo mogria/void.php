718ceb091d897596cbc8c0087bb8ce2543dae5b5
Mogria <m0gr14@gmail.com>
2012-06-21 18:05:10 +0200

    added some comments to the Role system

--- lib/Role/RoleBase.php
+++ lib/Role/RoleBase.php
@@ -2,16 +2,74 @@
 
 namespace Void;
 
+/**
+ * abstract base class for all the user roles
+ * 
+ * if you want to create your own role, create a new class in the roles/ folder
+ * and extend this class. You can also base a new role on one of your Role classes.
+ *
+ * inside the class you can set static properties to true. every static property set to true
+ * stands for somewhing the user can do if he has the role. you can call the 
+ * allowedTo method to get this
+ *
+ * you can also disallow the user something if you set a static property to false
+ *
+ * a little example
+ *
+ * <code>
+ * <?php
+ *
+ * class MyUserRole extends RoleBase {
+ *   // an unique id is needed for the RoleManager
+ *   static $id = 3;
+ *   static $comment = true;
+ *   static $vote = true;
+ * }
+ *
+ * class MyAdminRole extends MyUserRole {
+ *   static $id = 1;
+ *   static $administrate = true;
+ *   static $vote = false;
+ * }
+ *
+ * $user = new MyUserRole();
+ * $admin = new MyAdminRole();
+ *
+ * $user->allowedTo("comment"); // true
+ * $user->allowedTo("vote"); // true
+ * $user->allowedTo("administrate"); // false, because not set
+ * $user->allowedTo("destroy_database"); // false, because not set
+ *
+ *
+ * $admin->allowedTo("comment"); // true, because inheritance
+ * $admin->allowedTo("vote"); // false, because set to false
+ * $admin->allowedTo("administrate"); // true
+ * $admin->allowedTo("destroy_database"); // false, because not set
+ * </code>
+ */
 abstract class RoleBase {
+  /**
+   * must be set to an unique id for every subclass
+   * @var int $id
+   */
   protected static $id;
 
-  public static $registred = false;
-
+  /**
+   * Constructor
+   * registers itself to the RoleManager (if not already done)
+   *
+   * if you want to create a role class, that doesn't register to the RoleManager,
+   * simply overwrite this Method without calling parent::__construct()
+   */
   public function __construct() {
     static::register($this);
   }
 
-  public static function register(RoleBase $role) {
+  /**
+   * registers this class to the RoleManager (only one time)
+   * @param RoleBase $this - an instance of this object
+   */
+  protected static function register(RoleBase $role) {
     static $registred = false;
     if(!$registred) {
       $registred = true;
@@ -19,6 +77,13 @@ abstract class RoleBase {
     }
   }
 
+  /**
+   * returns (true/false) wheter a user of the given
+   * role is allowed to do something ($right_name)
+   *
+   * @param the name of the thing the role is (not) allowed to do
+   * @return bool
+   */
   public function allowedTo($right_name) {
     return isset(static::${$right_name}) ? static::${$right_name} : false;
   }
--- lib/Role/RoleManager.php
+++ lib/Role/RoleManager.php
@@ -2,9 +2,24 @@
 
 namespace Void;
 
+/**
+ * gives every role an id
+ * you can use the get()-Method to get an role by id
+ */
 class RoleManager {
+  /**
+   * an array of all roles who have been registered yet
+   * @static
+   * @var $roles
+   */
   private static $roles = Array(); 
 
+  /**
+   * checks if $id is an valid id
+   * if not an VoidException is thrown
+   *
+   * @param $id
+   */
   protected static function validateId($id) {
     if(!is_int($id)) {
       throw new VoidException("Invalid role id format: has to be an integer");
@@ -15,12 +30,26 @@ class RoleManager {
     }
 
   }
+
+  /**
+   * registers a new role class
+   *
+   * @param $id an unique id
+   * @param $role an instance of the new role class
+   */
   public static function register($id, RoleBase $role) {
     self::validateId($id);
     self::$roles[$id] = $role;
   }
 
+  /**
+   * returns the role with the given $id
+   * if none exists null
+   *
+   * @param $id
+   * @return RoleBase
+   */
   public static function get($id) {
-    return self::$roles[$id];
+    return isset(self::$roles[$id]) ? self::$roles[$id] : null;
   }
 }
