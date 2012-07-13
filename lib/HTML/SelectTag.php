189ab84b64600528531191f0f39cb3312ef3185c
Mogria <m0gr14@gmail.com>
2012-07-13 08:28:31 +0200

    you can now pass a list of models to the select Tag, wrote tests

--- lib/HTML/SelectTag.php
+++ lib/HTML/SelectTag.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace Void\HTML;
+use \ActiveRecord\Model;
 
 
 class SelectTag extends InputTag {
   protected static $tag_name = "select";
+  protected $label_system;
 
-  public function __construct($name, $content, Array $attributes = Array()) {
+  public function __construct($name, $content, $label = Array(), Array $attributes = Array()) {
+    $this->label_system = $label;
     parent::__construct($name, "select", $content, $attributes);
     $this->exists('type') && $this->delete('type');
   }
@@ -21,12 +24,21 @@ class SelectTag extends InputTag {
 
   public function setContent($content) {
     $this->content = Array();
-    $keys = array_values($content) == $content;
+    $keys = array_values($content) === $content;
     foreach($content as $key => $value) {
-      if($keys) {
-        $this->content[] = new OptionTag($value);
+      if($value instanceof Model) {
+        $label = array_values($this->label_system);
+        $format = array_shift($label);
+        foreach($label as &$label_value) {
+          $label_value = $value->$label_value;
+        }
+        $this->content[] = new OptionTag($value->{$value->get_primary_key(true)}, call_user_func_array('sprintf', array_merge(array($format), $label)));
       } else {
-        $this->content[] = new OptionTag($key, $value);
+        if($keys) {
+          $this->content[] = new OptionTag($value);
+        } else {
+          $this->content[] = new OptionTag($key, $value);
+        }
       }
     }
   }
