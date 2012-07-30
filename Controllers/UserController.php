b2f5efce65945320f831d4aa3eafcc3d7bd0592d
Mogria <m0gr14@gmail.com>
2012-07-30 17:49:00 +0200

    generated UserController

---
+++ Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Void;
+
+class UserController extends ApplicationController {
+
+  public function action_index() {
+  }
+  public function action_login() {
+  }
+  public function action_logout() {
+  }
+  public function action_show() {
+  }
+  public function action_new() {
+  }
+  public function action_edit() {
+  }
+  public function action_delete() {
+  }
+
+}
---
+++ Helpers/UserHelper.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace Void;
+
+class userHelper extends ApplicationHelper {
+}
