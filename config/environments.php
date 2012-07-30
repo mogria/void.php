fe396218b88b020fda13825426f9024ae1563070
Mogria <m0gr14@gmail.com>
2012-07-30 08:37:23 +0200

    fixed errors, if you require config/environments multiple times

--- config/environments.php
+++ config/environments.php
@@ -13,43 +13,45 @@ require_once dirname(__FILE__) . DS . 'default_configuration.php';
 $environment = DEVELOPMENT;
 
 
-/**
- * Returns the Configuration with ALL THE settings for each environment
- */
-function getConfig() {
-  // get the variable $environment from outside
-  global $environment;
+if(!function_exists('\Void\getConfig')) {
+  /**
+   * Returns the Configuration with ALL THE settings for each environment
+   */
+  function getConfig() {
+    // get the variable $environment from outside
+    global $environment;
 
-  // create the Config object and return it
-  return new Config($environment, function($cfg) {
-    
-    // loading the default configuration into ALL the environments
-    loadDefaultConfig($cfg);
-    
-    // the settings for all environments
-    $cfg->config(function($cfg) {
-      // the default Controller (if none is given)
-      $cfg->dispatcher_default_controller = "Posts";
-    }, 'all');
+    // create the Config object and return it
+    return new Config($environment, function($cfg) {
+      
+      // loading the default configuration into ALL the environments
+      loadDefaultConfig($cfg);
+      
+      // the settings for all environments
+      $cfg->config(function($cfg) {
+        // the default Controller (if none is given)
+        $cfg->dispatcher_default_controller = "Posts";
+      }, 'all');
 
-    // the settings just for the development environment
-    $cfg->config(function($cfg) {
-      /** database configuration **/
-      $cfg->modelconfig_connection = 'mysql://root@localhost/voidphp_test_blog';
-    }, DEVELOPMENT);
-
-
-    // the settings for the test environment
-    $cfg->config(function($cfg) {
-      $cfg->modelconfig_connection = 'mysql://root@localhost/voidphp_test_blog';
-    }, TEST);
+      // the settings just for the development environment
+      $cfg->config(function($cfg) {
+        /** database configuration **/
+        $cfg->modelconfig_connection = 'mysql://root@localhost/voidphp_test_blog';
+      }, DEVELOPMENT);
 
 
-    // the settings for the production environment
-    $cfg->config(function($cfg) {
-      $cfg->modelconfig_connection = 'mysql://root@localhost/voidphp_test_blog';
-    }, PRODUCTION);
-  });
+      // the settings for the test environment
+      $cfg->config(function($cfg) {
+        $cfg->modelconfig_connection = 'mysql://root@localhost/voidphp_test_blog';
+      }, TEST);
+
+
+      // the settings for the production environment
+      $cfg->config(function($cfg) {
+        $cfg->modelconfig_connection = 'mysql://root@localhost/voidphp_test_blog';
+      }, PRODUCTION);
+    });
+  }
 }
 
 $config = getConfig();
