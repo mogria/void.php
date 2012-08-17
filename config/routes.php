944eaa86b653a5c5a47bc6a333cb8ed7687a1487
Mogria <m0gr14@gmail.com>
2012-08-17 18:18:47 +0200

    added a math method in the pages controller to do some match using the routing system ;-)

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,10 @@ namespace Void;
 
 Router::configure(function($route) {
   $route->match('/', '/pages', 'root');
+  $route->match('/:[0-9\.]++numbers', '/pages/math/+/:numbers', 'add');
+  $route->match('/:[0-9\.]+-numbers', '/pages/math/-/:numbers', 'sub');
+  $route->match('/:[0-9\.]+*numbers', '/pages/math/*/:numbers', 'mul');
+  $route->match('/:[0-9\.]+:numbers', '/pages/math/:/:numbers', 'div');
   $route->match('/:[0-9]{2,3}nr', '/pages/about/:nr');
   $route->match('/:page', '/pages/:page');
   $route->match('/_:controller', '/:controller');
