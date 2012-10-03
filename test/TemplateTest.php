858132fb2766b21e41cd1a4f109c76076bfadfa7
Mogria <m0gr14@gmail.com>
2012-10-03 14:49:53 +0200

    fixed TemplateTest by removing test for parse (this code has moved to the renderer classes)

--- test/TemplateTest.php
+++ test/TemplateTest.php
@@ -14,23 +14,6 @@ class TemplateTest extends \PHPUnit_Framework_TestCase {
     $this->template = new Template('layout/test'/*, Array('included' => new Template('included')) */);
   }
 
-  public function testParse() {
-    $expected_output = <<<TEXT
-<h1>This is just a Test Template</h1>
-<?php for(\$x = 0; \$x < 2; \$x++): ?>
-
-<p>This is just some random Text to Test the Template Engine. <?php echo date('d.m.Y', 0) ?>
-</p>
-<?php endfor ?>
-
-<?php echo \$included->render() ?>
-
-<p>encoded: <?php echo htmlspecialchars("<>\"", ENT_QUOTES, 'UTF-8') ?>
-</p>
-TEXT;
-    $this->assertEquals($expected_output, $this->template->parse(file_get_contents($this->template->getFile())));
-  }
-
   public function testRender() {
     $expected_output = <<<TEXT
 <h1>This is just a Test Template</h1>
