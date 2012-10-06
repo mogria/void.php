df824d5326257c51d85a174f98127e740bcecd74
Mogria <m0gr14@gmail.com>
2012-10-06 16:17:54 +0200

    updated test database (missing column role)

--- test/db/voidphp_test.sql
+++ test/db/voidphp_test.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.56, for pc-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.1.62, for pc-linux-gnu (i686)
 --
 -- Host: localhost    Database: voidphp_test
 -- ------------------------------------------------------
--- Server version	5.1.56-log
+-- Server version	5.1.62-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,35 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `voidphp_test`
+-- Table structure for table `users`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `voidphp_test` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `voidphp_test`;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `password` varchar(161) NOT NULL,
+  `role` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `users`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -56,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-07-10 18:25:48
+-- Dump completed on 2012-10-06 16:17:28
